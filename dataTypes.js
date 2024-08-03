b655a1c2fb668ca4d3d658bd9366a41f2981c9c0
Adeela Shahid <92438784+AdeelaShahid@users.noreply.github.com>
2024-08-03 12:07:43 +0530

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,25 @@
+const score =90
+const scoreValue = 90.8  
+
+const isLogegdIn = false
+const outsideTemp = null
+let userEmail;  //undefined
+
+const id = Symbol('123') //unique
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+//array, objects, functions
+const heros = ["shaktiman", "superman", "spiderman"];
+let myObj = {
+    name: "Adeela",
+    age: 22,
+
+}
+
+const myFunction = function(){
+    console.log("Hello");
+
+}
+myFunction.call(); 
