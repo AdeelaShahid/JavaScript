cdd26fd1c6bfd15f583eb1288be06741d7809899
Adeela Shahid <92438784+AdeelaShahid@users.noreply.github.com>
2024-08-03 12:08:02 +0530

    Add files via upload

---
+++ Datatype_conversion.js
@@ -0,0 +1,12 @@
+let score = 44
+console.log(typeof score);
+//console.log(typeof (score));
+let scoreStr = String(score);
+console.log(scoreStr);
+console.log(typeof scoreStr)
+
+ let name = "adeela"
+ console.log(typeof name);
+ let nameNUm = Number(name);
+ console.log(nameNUm);
+ console.log(typeof nameNUm)
