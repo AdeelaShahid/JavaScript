9019573cf165f85b400703fc9efec536f0756750
Adeela Shahid <92438784+AdeelaShahid@users.noreply.github.com>
2024-08-03 12:08:40 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,18 @@
+const accountId=1429304
+let accountEmail = "adeela@123.com"
+var accountPassword = "1234"
+accountCity = "Hyderabad"
+let accountState;
+let accountColor;
+
+//update data
+
+
+
+accountEmail = "iluuu@12"
+accountPassword = "5901"
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState, accountColor])
+
+//console.log(accountCity);
