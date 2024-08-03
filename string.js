fe491bcda5a0dee21adc93f8e3dd1dcb75a4e261
Adeela Shahid <92438784+AdeelaShahid@users.noreply.github.com>
2024-08-03 12:01:54 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,24 @@
+const name = "ADEELA"
+const count = 20
+
+
+//console.log(name + count);
+
+console.log(`my name is ${name} and my count is ${count}`)
+
+console.log(name.charAt(2));
+console.log(name.indexOf('D'));
+console.log(name.toLowerCase())
+
+const newString = name.substring(0,3);
+console.log(newString)
+console.log(newString.length)
+
+
+
+const myName = new String ('shahid')
+console.log(myName.toUpperCase())
+console.log(myName.length);
+
+const anotherString = name.slice(1, 4)
+console.log(anotherString);
