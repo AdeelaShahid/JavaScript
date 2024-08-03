787df70dd8ebd55b2e1aa805383d2d6fe7806970
Adeela Shahid <92438784+AdeelaShahid@users.noreply.github.com>
2024-08-03 12:06:32 +0530

    Add files via upload

---
+++ num_maths.js
@@ -0,0 +1,9 @@
+const score = 100
+console.log(score)
+
+const balance = new Number(20.870)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+console.log(balance.toPrecision(2))
